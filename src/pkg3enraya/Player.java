0c33e5f23f3d8f0e5028c32954864391d7241db7
Adriaaltemir <Adriaaltemir18@gmail.com>
2018-05-05 17:36:31 +0200

    Structura proyecto

---
+++ src/pkg3enraya/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3enraya;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author usuario
+ */
+public class Player {
+   
+}
