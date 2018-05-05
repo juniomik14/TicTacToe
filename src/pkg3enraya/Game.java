41fb1a35dc615d168c12e353fdd00dff0380893d
Adriaaltemir <Adriaaltemir18@gmail.com>
2018-05-05 17:45:46 +0200

    Create board

--- src/pkg3enraya/Game.java
+++ src/pkg3enraya/Game.java
@@ -20,8 +20,28 @@ public class Game {
     private Cell[][] cells;
     private ArrayList<Player> players;
 
-   
-    
+    public Game() {
+        this.cells = createBoard();
+        this.players = insertPlayers();
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public Cell[][] createBoard() {
+        int id = 1;
+        Cell[][] cells = new Cell[3][3];
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(false, id);
+                id++;
+            }
+        }
+
+        return cells;
+    }
 
     
 }
