0b1db27a84519694b3eb27e07a0002b997fc1c16
Adriaaltemir <Adriaaltemir18@gmail.com>
2018-05-05 17:50:52 +0200

    arraylist player

--- src/pkg3enraya/Game.java
+++ src/pkg3enraya/Game.java
@@ -43,5 +43,36 @@ public class Game {
         return cells;
     }
 
+    public void showBoard() {
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                System.out.print(cells[i][j].getForm() + " ");
+            }
+
+            System.out.println(" ");
+        }
+    }
+
+    public ArrayList<Player> insertPlayers() {
+        Scanner sc = new Scanner(System.in);
+        Player player1;
+        Player player2;
+        String auxName;
+        ArrayList<Player> players = new ArrayList<>();
+
+        System.out.print("Nombre del primer jugador: ");
+        auxName = sc.nextLine();
+        player1 = new Player(auxName, "O");
+
+        System.out.print("Nombre del segundo jugador: ");
+        auxName = sc.nextLine();
+        player2 = new Player(auxName, "X");
+
+        players.add(player1);
+        players.add(player2);
+
+        return players;
+    }
+
     
 }
