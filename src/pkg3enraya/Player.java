50da2504e87eeb5da1e57841551ec39048d6dc2c
Adriaaltemir <Adriaaltemir18@gmail.com>
2018-05-05 17:40:55 +0200

    Estructura player

--- src/pkg3enraya/Player.java
+++ src/pkg3enraya/Player.java
@@ -18,5 +18,59 @@ import java.util.Scanner;
  * @author usuario
  */
 public class Player {
-   
+    private String name;
+    private String selection;
+    private ArrayList<Cell> cellsSelected;
+    private int victories;
+
+    public Player(String name, String selection) {
+        this.name = name;
+        this.selection = selection;
+        this.victories = 0;
+        this.cellsSelected = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Cell> getCellsSelected() {
+        return cellsSelected;
+    }
+
+    public String getSelection() {
+        return selection;
+    }
+    
+    
+    public int getCellsQty() {
+        return cellsSelected.size();
+    }
+
+    public void addVictory() {
+        this.victories++;
+    }
+
+    public int getVictories() {
+        return victories;
+    }
+    private int getPosicion(){
+        int posX;
+          Scanner sc = new Scanner(System.in);
+          
+       
+        
+        try{
+        posX = sc.nextInt();
+        
+        }catch(Exception e){
+            throw new IllegalArgumentException(e);
+        }
+        if(posX<0||posX>2){
+            System.out.println("Posicion entre 0 y 2");
+            throw new IllegalArgumentException();
+            
+        }
+        return posX;
+    }
 }
