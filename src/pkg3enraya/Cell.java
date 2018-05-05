b97232ed2f491c54a8aecc5ed6c00816abbf189d
Adriaaltemir <Adriaaltemir18@gmail.com>
2018-05-05 17:39:00 +0200

    Estructura cell

--- src/pkg3enraya/Cell.java
+++ src/pkg3enraya/Cell.java
@@ -10,5 +10,30 @@ package pkg3enraya;
  * @author usuario
  */
 public class Cell {
-   
+    private boolean inUse;
+    private String form;
+    private int id;
+
+    public Cell(boolean inUse, int id) {
+        this.inUse = inUse;
+        this.form = "*";
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setInUse(String form) {
+        this.inUse = true;
+        this.form = form;
+    }
+
+    public String getForm() {
+        return form;
+    }
 }
